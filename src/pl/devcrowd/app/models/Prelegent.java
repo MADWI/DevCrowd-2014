cd9cf6ac1fff4f072abfe2518edafd52002c1822
sebap123 <sebastian.peryt@gmail.com>
2014-03-03 19:20:30 +0100

    [ADD] Prelegent i Presentation class.

---
+++ src/pl/devcrowd/app/models/Prelegent.java
@@ -0,0 +1,57 @@
+package pl.devcrowd.app.models;
+
+public class Prelegent {
+	
+	private String name;
+	private String photoPath;
+	private String description;
+	
+	Prelegent(String name, String photoPath, String description){
+		this.setName(name);
+		this.setPhotoPath(photoPath);
+		this.setDescription(description);
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the photoPath
+	 */
+	public String getPhotoPath() {
+		return photoPath;
+	}
+
+	/**
+	 * @param photoPath the photoPath to set
+	 */
+	public void setPhotoPath(String photoPath) {
+		this.photoPath = photoPath;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
---
+++ src/pl/devcrowd/app/models/Presentation.java
@@ -0,0 +1,84 @@
+package pl.devcrowd.app.models;
+
+public class Presentation {
+	
+	private String hourStart;
+	private String hourEnd;
+	private String room;
+	private String description;
+	private String title;
+	private String prelegent;
+	
+	
+	public Presentation(String hourStart, String hourEnd, String room,
+			String description, String title, String prelegent) {
+		super();
+		this.hourStart = hourStart;
+		this.hourEnd = hourEnd;
+		this.room = room;
+		this.description = description;
+		this.title = title;
+		this.prelegent = prelegent;
+	}
+
+
+	public String getHourStart() {
+		return hourStart;
+	}
+
+
+	public void setHourStart(String hourStart) {
+		this.hourStart = hourStart;
+	}
+
+
+	public String getHourEnd() {
+		return hourEnd;
+	}
+
+
+	public void setHourEnd(String hourEnd) {
+		this.hourEnd = hourEnd;
+	}
+
+
+	public String getRoom() {
+		return room;
+	}
+
+
+	public void setRoom(String room) {
+		this.room = room;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public String getPrelegent() {
+		return prelegent;
+	}
+
+
+	public void setPrelegent(String prelegent) {
+		this.prelegent = prelegent;
+	}
+	
+}
