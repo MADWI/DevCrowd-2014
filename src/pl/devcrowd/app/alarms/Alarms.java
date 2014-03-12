64b8f3f406eb515cde8e927c0a0235b9a4847af5
Michal <michal-sajmon@wp.pl>
2014-03-12 19:17:58 +0100

    [ADD] Method to cancel setted alarm

--- src/pl/devcrowd/app/alarms/Alarms.java
+++ src/pl/devcrowd/app/alarms/Alarms.java
@@ -1,5 +1,6 @@
 package pl.devcrowd.app.alarms;
 
+import pl.devcrowd.app.utils.DebugLog;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -15,11 +16,20 @@ public class Alarms {
 	}
 	
 	public void setAlarm(int lessonID, long time){
-		alarmManager = (AlarmManager) ctx.getSystemService(ctx.ALARM_SERVICE);
+		alarmManager = (AlarmManager) ctx.getSystemService(Context.ALARM_SERVICE);
 		Intent intent = new Intent("AlarmReceiver");
 		intent.putExtra("lessonID", lessonID);
 		PendingIntent pendingIntent = PendingIntent.getBroadcast(ctx, lessonID, intent, PendingIntent.FLAG_ONE_SHOT);
 		alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, time, 0, pendingIntent);
+		DebugLog.d("Alarm for ID: " + lessonID + "is set on now!");
+	}
+	
+	public void cancelAlarm(int lessonID){
+		Intent intent = new Intent("AlarmReceiver");
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(ctx, lessonID, intent, 0);
+		alarmManager = (AlarmManager) ctx.getSystemService(Context.ALARM_SERVICE);
+		alarmManager.cancel(pendingIntent);
+		DebugLog.d("Alarm for ID: " + lessonID + "was canceled!");
 	}
 
 }
