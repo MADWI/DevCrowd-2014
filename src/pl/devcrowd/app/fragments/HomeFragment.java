158b9b3011db936cf88c8238cf0f8a669d44678a
Michal <michal-sajmon@wp.pl>
2014-03-21 23:33:35 +0100

    [ADD] imgLogoDevCrowd onClick
    OnClick logo on app MainScreen switch fragment to presentations list.

--- src/pl/devcrowd/app/fragments/HomeFragment.java
+++ src/pl/devcrowd/app/fragments/HomeFragment.java
@@ -3,11 +3,17 @@ package pl.devcrowd.app.fragments;
 import pl.devcrowd.app.R;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements OnClickListener {
+	
+	private ImageView imgLogoDevCrowd;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -20,8 +26,26 @@ public class HomeFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_home, container, false);
+		
+		imgLogoDevCrowd = (ImageView) view.findViewById(R.id.imgLogoDevCrowd);
+		imgLogoDevCrowd.setOnClickListener(this);
 
 		return view;
+	}
+
+	@Override
+	public void onClick(View view) {
+		// TODO Auto-generated method stub
+		switch(view.getId()){
+			case R.id.imgLogoDevCrowd:
+				Fragment fragment = new ScheduleHostFragment();
+				FragmentManager fragmentManager = getFragmentManager();
+				FragmentTransaction fragmentTransaction = fragmentManager
+						.beginTransaction();
+				fragmentTransaction.replace(R.id.frame_container, fragment);
+				fragmentTransaction.commit();
+				break;
+		}
 	}
 
 }
