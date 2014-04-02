53c9b949023aad70e725c67429d5380fd83429dc
tof3r <dawidg90@gmail.com>
2014-04-02 21:57:18 +0200

    About Dialog clickable logos

--- src/pl/devcrowd/app/dialogs/AboutDialog.java
+++ src/pl/devcrowd/app/dialogs/AboutDialog.java
@@ -1,12 +1,21 @@
 package pl.devcrowd.app.dialogs;
 
-import eu.inmite.android.lib.dialogs.BaseDialogFragment;
 import pl.devcrowd.app.R;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import eu.inmite.android.lib.dialogs.BaseDialogFragment;
 
-public class AboutDialog extends BaseDialogFragment {
+public class AboutDialog extends BaseDialogFragment implements OnClickListener {
+
+	private int website = 0;
+	private final String ACTION = "android.intent.action.VIEW";
+	private final String urlDevCrowd = "http://2014.devcrowd.pl";
+	private final String urlAuthors = "http://www.mad.zut.edu.pl";
 
 	public static void show(FragmentActivity activity, String tag) {
 		AboutDialog dialog = new AboutDialog();
@@ -17,6 +26,13 @@ public class AboutDialog extends BaseDialogFragment {
 	protected Builder build(Builder initialBuilder) {
 		LayoutInflater inflater = LayoutInflater.from(getActivity());
 		View aboutView = inflater.inflate(R.layout.about_dialog_layout, null);
+
+		ImageView logoDevCrowd, logoMAD;
+		logoDevCrowd = (ImageView) aboutView.findViewById(R.id.devCrowdLogo);
+		logoDevCrowd.setOnClickListener(this);
+
+		logoMAD = (ImageView) aboutView.findViewById(R.id.authorsLogo);
+		logoMAD.setOnClickListener(this);
 
 		initialBuilder.setView(aboutView).setTitle(R.string.about_dialog_title)
 				.setNegativeButton(R.string.cancel, new View.OnClickListener() {
@@ -30,4 +46,31 @@ public class AboutDialog extends BaseDialogFragment {
 		return initialBuilder;
 	}
 
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.devCrowdLogo:
+			website = 1;
+			launchWebsite(website);
+			break;
+
+		case R.id.authorsLogo:
+			website = 2;
+			launchWebsite(website);
+			break;
+		}
+	}
+
+	private void launchWebsite(int website) {
+
+		if (website == 1) {
+			Intent intent = new Intent(ACTION, Uri.parse(urlDevCrowd));
+			startActivity(intent);
+		}
+
+		if (website == 2) {
+			Intent intent = new Intent(ACTION, Uri.parse(urlAuthors));
+			startActivity(intent);
+		}
+	}
 }
